0486ccbd66c007d0d9ce4bc62ccd9df44555816a
andres <fcortesbio@gmail.com>
2025-09-04 15:49:12 -0500

    update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const port = process.env.PORT ?? 3001;
 // Use the express.static middleware to serve files from the current directory
 app.use(express.static(__dirname));
 
-// Serve the static HTML file
+// A simple health check endpoint
 app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "index.html"));
+  res.status(200).send("Hello from Health!");
 });
 
-// // A simple health check endpoint
-// app.get("/health", (req, res) => {
-//   res.status(200).send("Hello from Health!");
-// });
+// Serve the static HTML file
+app.get("/health", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "index.html"));
+});
 
 // Start the server
 app.listen(port, () => {
