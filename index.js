674c59b86ec3da1f3506c8db2c7c855bf5c1797b
andres <fcortesbio@gmail.com>
2025-09-04 15:59:47 -0500

    testing

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = process.env.PORT ?? 3001;
 // Use the express.static middleware to serve files from the current directory
 app.use(express.static(__dirname));
 
-// A simple health check endpoint
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Health!");
-});
+// // A simple health check endpoint
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello from Health!");
+// });
 
 // Serve the static HTML file
 app.get("/health", (req, res) => {
